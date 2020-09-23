5f798770af2a5c6425a50b399f6ad17342449da2
Luan-Augusto <luanalencar134@gmail.com>
2020-09-23 18:40:09 -0300

    add metodos:Spotify/Musica

--- projeto-spotify-encapsulado/src/in/augustluan/entidades/Musica.java
+++ projeto-spotify-encapsulado/src/in/augustluan/entidades/Musica.java
@@ -9,5 +9,75 @@ public class Musica {
 	private int anoLancamento;
 	private double duracao;
 	private int vezesReproduzido;
-	
+
+	public Musica(String nome, String artista, String letra, Enum genero, int anoLancamento, double duracao,
+			int vezesReproduzido) {
+		this.nome = nome;
+		this.artista = artista;
+		this.letra = letra;
+		this.genero = genero;
+		this.anoLancamento = anoLancamento;
+		this.duracao = duracao;
+		this.vezesReproduzido = vezesReproduzido;
+	}
+
+	public Musica() {
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getArtista() {
+		return artista;
+	}
+
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+
+	public String getLetra() {
+		return letra;
+	}
+
+	public void setLetra(String letra) {
+		this.letra = letra;
+	}
+
+	public Enum getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Enum genero) {
+		this.genero = genero;
+	}
+
+	public int getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(int anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public double getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(double duracao) {
+		this.duracao = duracao;
+	}
+
+	public int getVezesReproduzido() {
+		return vezesReproduzido;
+	}
+
+	public void setVezesReproduzido(int vezesReproduzido) {
+		this.vezesReproduzido = vezesReproduzido;
+	}
+
 }
--- projeto-spotify-encapsulado/src/in/augustluan/entidades/Spotify.java
+++ projeto-spotify-encapsulado/src/in/augustluan/entidades/Spotify.java
@@ -1,5 +1,34 @@
 package in.augustluan.entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Spotify {
+
+	private List<Musica> musicas;
+
+	public Spotify(List<Musica> musicas) {
+		this.musicas = new ArrayList<>();
+	}
+
+	public List<Musica> getMusicas() {
+		return musicas;
+	}
+
+	public void setMusicas(List<Musica> musicas) {
+		this.musicas = musicas;
+	}
+
+	public void adicionarMusica(Musica musica) {
+		musicas.add(musica);
+	}
+
+	public int quantidadeMusica(int quantidade) {
+		int mus = 0;
+		for (int i = 0; i < musicas.size(); i++) {
+			mus = i;
+		}
+		return mus;
+	}
 
 }
