8ae65f8166320e1b94f48d213581a22617a55cdb
Luan-Augusto <luanalencar134@gmail.com>
2020-09-23 18:00:50 -0300

    criacao Enum e Spotify

---
+++ projeto-spotify-encapsulado/src/in/augustluan/entidades/Enum.java
@@ -0,0 +1,6 @@
+package in.augustluan.entidades;
+
+public enum Enum {
+
+	
+}
--- projeto-spotify-encapsulado/src/in/augustluan/entidades/Musica.java
+++ projeto-spotify-encapsulado/src/in/augustluan/entidades/Musica.java
@@ -2,4 +2,12 @@ package in.augustluan.entidades;
 
 public class Musica {
 
+	private String nome;
+	private String artista;
+	private String letra;
+	private Enum genero;
+	private int anoLancamento;
+	private double duracao;
+	private int vezesReproduzido;
+	
 }
