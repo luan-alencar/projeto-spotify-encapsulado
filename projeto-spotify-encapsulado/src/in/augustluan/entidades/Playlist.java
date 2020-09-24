e309720895db1efdcfb553d3effab88e6788f0dc
Luan-Augusto <luanalencar134@gmail.com>
2020-09-24 00:01:21 -0300

    testando add()

--- projeto-spotify-encapsulado/src/in/augustluan/entidades/Playlist.java
+++ projeto-spotify-encapsulado/src/in/augustluan/entidades/Playlist.java
@@ -31,6 +31,27 @@ public class Playlist {
 	public static void main(String[] args) {
 		Playlist p = new Playlist();
 		Musica m = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m2 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m3 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m4 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m5 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m6 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m7 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m8 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m9 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m10 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
+		Musica m11 = new Musica("asdf", "sdfa", "scvv", null, 2020, 45.04, 2);
 		p.add(m);
+		p.add(m2);
+		p.add(m3);
+		p.add(m4);
+		p.add(m5);
+		p.add(m6);
+		p.add(m7);
+		p.add(m8);
+		p.add(m9);
+		p.add(m10);
+		p.add(m11);
+
 	}
 }
